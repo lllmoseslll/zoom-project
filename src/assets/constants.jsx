a8e7752aa9ad96624f62ff10eeebf7e08db6f997
lllmoseslll <mosespeter620@gmail.com>
2024-04-16 03:14:39 +0300

    first commit

---
+++ src/assets/constants.jsx
@@ -0,0 +1,220 @@
+
+export const ZoomMobileLogo = () => (
+	
+	<svg
+			aria-label='Zoom'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		width='24'
+
+		id="Layer_1" version="1.1" viewBox="0 0 512 512" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 512 512"><g id="Face_ID"><circle cx="256" cy="259.7" id="Backround" r="215.6" fill="#ffca38" className="fill-eeedf2"></circle><g id="Eye">
+			<title></title>
+		<path d="M417.3 259.1c.2.3.2.8 0 1.1-36.1 60.3-95 99.6-161.5 99.6-66.2 0-124.5-38.3-160.7-98.2-.7-1.2-.7-2.8 0-3.9 36.2-59.5 94.7-98.2 160.7-98.2 66 0 123.8 37.2 160 96.7l1.5 2.9z" fill="#eb3317" className="fill-d3d5e7"></path><g id="Sparkles_15_"></g><path d="M397.2 259.3c.2.2.2.6 0 .8-31.6 44.3-83.2 73.1-141.4 73.1-58 0-109-28.1-140.7-72h0c-.6-.9-.6-2 0-2.9 31.7-43.7 82.9-72 140.7-72 57.8 0 108.4 27.3 140.1 70.9l1.3 2.1z" id="Shape_63_" fill="#ffffff" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" className="fill-ffffff stroke-1e247e"></path><path d="M349 225.3c5 3 9.7 6.5 14 10.4" id="Line_22_" fill="none" stroke="#801e1e" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" className="stroke-1e247e"></path><g id="Cornea"><circle cx="256" cy="259.7" id="Iris" r="73.5" fill="#c8fa00" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" className="fill-69aef8 stroke-1e247e"></circle><circle cx="256" cy="259.7" id="Pupil" r="27" fill="#801e1e" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" className="fill-1e247e stroke-1e247e"></circle><circle cx="274.3" cy="232.7" id="Blink" r="18.3" fill="#ffffff" className="fill-ffffff"></circle><g id="Vertical_line_3_"><path d="M320 256.3c.1 1 .1 2 .1 3.1 0 5.6-1.1 11-3.2 15.8" id="Bottom_51_" fill="none" stroke="#801e1e" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" className="stroke-1e247e"></path><path d="M317.7 245.7c.6 1.7 1.1 3.4 1.5 5.2" id="Top_53_" fill="none" stroke="#801e1e" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" className="stroke-1e247e"></path></g></g><g id="Lines_29_"></g></g><g id="Corner_lines"><g id="Bottom_right"><path d="M400 368.3c0 16.4-13.3 29.7-29.7 29.7" id="Bottom_49_" fill="none" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path><path d="M389.1 373c0 7.4-6 13.4-13.4 13.4" id="Top_51_" fill="none" stroke="#801e1e" strokeWidth="2.5" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path></g><g id="Top_right"><path d="M388.8 145.9c0-7.4-6-13.4-13.4-13.4" id="Bottom_48_" fill="none" stroke="#801e1e" strokeWidth="2.5" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path><path d="M370.3 121.4c16.4 0 29.7 13.3 29.7 29.7" id="Top_50_" fill="none" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path></g><g id="Bottom_left"><path d="M140.8 398c-16.4 0-29.7-13.3-29.7-29.7" id="Bottom_47_" fill="none" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path><path d="M122.9 373c0 7.4 6 13.4 13.4 13.4" id="Top_49_" fill="none" stroke="#801e1e" strokeWidth="2.5" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path></g><g id="Top_left"><path d="M122.5 145.9c0-7.4 6-13.4 13.4-13.4" id="Bottom_46_" fill="none" stroke="#801e1e" strokeWidth="2.5" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path><path d="M111.1 151c0-16.4 13.3-29.7 29.7-29.7" id="Top_48_" fill="none" stroke="#801e1e" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" className="stroke-1e247e"></path></g></g></g></svg>
+);
+
+export const SearchLogo = () => (
+	<svg
+		aria-label='Search'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		viewBox='0 0 24 24'
+		width='24'
+	>
+		<path
+			d='M19 10.5A8.5 8.5 0 1 1 10.5 2a8.5 8.5 0 0 1 8.5 8.5Z'
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+		></path>
+		<line
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			x1='16.511'
+			x2='22'
+			y1='16.511'
+			y2='22'
+		></line>
+	</svg>
+);
+export const ReelsLogo = () => (
+	<svg
+		aria-label='Reels'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		viewBox='0 0 24 24'
+		width='24'
+	>
+		<line
+			fill='none'
+			stroke='currentColor'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			x1='2.049'
+			x2='21.95'
+			y1='7.002'
+			y2='7.002'
+		></line>
+		<line
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			x1='13.504'
+			x2='16.362'
+			y1='2.001'
+			y2='7.002'
+		></line>
+		<line
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			x1='7.207'
+			x2='10.002'
+			y1='2.11'
+			y2='7.002'
+		></line>
+		<path
+			d='M2 12.001v3.449c0 2.849.698 4.006 1.606 4.945.94.908 2.098 1.607 4.946 1.607h6.896c2.848 0 4.006-.699 4.946-1.607.908-.939 1.606-2.096 1.606-4.945V8.552c0-2.848-.698-4.006-1.606-4.945C19.454 2.699 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.546 2 5.704 2 8.552Z'
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+		></path>
+		<path
+			d='M9.763 17.664a.908.908 0 0 1-.454-.787V11.63a.909.909 0 0 1 1.364-.788l4.545 2.624a.909.909 0 0 1 0 1.575l-4.545 2.624a.91.91 0 0 1-.91 0Z'
+			fillRule='evenodd'
+		></path>
+	</svg>
+);
+
+export const MessagesLogo = () => (
+	<svg
+		aria-label='Messenger'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		viewBox='0 0 24 24'
+		width='24'
+	>
+		<path
+			d='M12.003 2.001a9.705 9.705 0 1 1 0 19.4 10.876 10.876 0 0 1-2.895-.384.798.798 0 0 0-.533.04l-1.984.876a.801.801 0 0 1-1.123-.708l-.054-1.78a.806.806 0 0 0-.27-.569 9.49 9.49 0 0 1-3.14-7.175 9.65 9.65 0 0 1 10-9.7Z'
+			fill='none'
+			stroke='currentColor'
+			strokeMiterlimit='10'
+			strokeWidth='1.739'
+		></path>
+		<path
+			d='M17.79 10.132a.659.659 0 0 0-.962-.873l-2.556 2.05a.63.63 0 0 1-.758.002L11.06 9.47a1.576 1.576 0 0 0-2.277.42l-2.567 3.98a.659.659 0 0 0 .961.875l2.556-2.049a.63.63 0 0 1 .759-.002l2.452 1.84a1.576 1.576 0 0 0 2.278-.42Z'
+			fillRule='evenodd'
+		></path>
+	</svg>
+);
+
+export const NotificationsLogo = () => (
+	<svg
+		aria-label='Notifications'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		viewBox='0 0 24 24'
+		width='24'
+	>
+		<path d='M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z'></path>
+	</svg>
+);
+
+export const UnlikeLogo = () => (
+	<svg
+		aria-label='Unlike'
+		color='rgb(255, 48, 64)'
+		fill='rgb(255, 48, 64)'
+		height='24'
+		role='img'
+		viewBox='0 0 48 48'
+		width='24'
+	>
+		<title>Unlike</title>
+		<path d='M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z'></path>
+	</svg>
+);
+
+export const CreatePostLogo = () => (
+	<svg
+		aria-label='New post'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		viewBox='0 0 24 24'
+		width='24'
+	>
+		<path
+			d='M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552Z'
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+		></path>
+		<line
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			x1='6.545'
+			x2='17.455'
+			y1='12.001'
+			y2='12.001'
+		></line>
+		<line
+			fill='none'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+			x1='12.003'
+			x2='12.003'
+			y1='6.545'
+			y2='17.455'
+		></line>
+	</svg>
+);
+
+export const CommentLogo = () => (
+	<svg
+		aria-label='Comment'
+		color='rgb(245, 245, 245)'
+		fill='rgb(245, 245, 245)'
+		height='24'
+		role='img'
+		viewBox='0 0 24 24'
+		width='24'
+	>
+		<title>Comment</title>
+		<path
+			d='M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z'
+			fill='none'
+			stroke='currentColor'
+			strokeLinejoin='round'
+			strokeWidth='2'
+		></path>
+	</svg>
+);
